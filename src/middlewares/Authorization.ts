b125123f7c431adefb205ff54c406f577330a060
Whermerson19 <whermersonc@gmail.com>
2021-01-09 14:57:13 -0300

    middleware of authentication

---
+++ src/middlewares/Authorization.ts
@@ -0,0 +1,62 @@
+import { Request, Response, NextFunction } from "express";
+
+import { verify } from "jsonwebtoken";
+
+import authConfig from "../config/auth";
+
+interface IPayload {
+  sub: string;
+  admin: boolean;
+}
+
+export default class Authorization {
+  public adminAuthorization(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): void {
+    const authHeaders = request.headers.authorization;
+
+    if (!authHeaders) throw new Error("JWT Token is missing");
+
+    const [, token] = authHeaders.split(" ");
+
+    const verifyToken = verify(token, authConfig.jwt.secret);
+    if (!verifyToken) throw new Error("Invalid token");
+
+    const { sub, admin } = verifyToken as IPayload;
+
+    if (!admin) throw new Error("Only admins can use it");
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  }
+
+  public clientAuthorization(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): void {
+    const authHeaders = request.headers.authorization;
+
+    if (!authHeaders) throw new Error("JWT Token is missing");
+
+    const [, token] = authHeaders.split(" ");
+
+    const verifyToken = verify(token, authConfig.jwt.secret);
+    if (!verifyToken) throw new Error("Invalid token");
+
+    const { sub, admin } = verifyToken as IPayload;
+
+    if (admin) throw new Error("Only clients can use it");
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  }
+}
